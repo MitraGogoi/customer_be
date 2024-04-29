f8609f2cbecbcc04127ae6e574dd8e66a85b88e9
codergogoi <codergogoi@gmail.com>
2024-04-29 15:43:28 +0200

    added internal MS

---
+++ checkout/src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Hello from checkout",
+  });
+});
+
+app.listen(8000, () => {
+  console.log("backend is lstening at 8000");
+});
--- graphql/src/index.js
+++ graphql/src/index.js
@@ -7,7 +7,13 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.status(200).json({
-    message: "Hello from customer backend",
+    message: "GraphQL Service",
+  });
+});
+
+app.get("/internal", (req, res) => {
+  return res.status(200).json({
+    message: "calling from internal backend ms",
   });
 });
 
