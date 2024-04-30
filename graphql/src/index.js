33f06d218b0b01b682feff857925e48976cef15a
codergogoi <codergogoi@gmail.com>
2024-04-30 13:53:06 +0200

    added servcie

--- graphql/src/index.js
+++ graphql/src/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
+  //devx-namespace.local
   return res.status(200).json({
     message: "GraphQL Service",
   });
 });
 
-app.get("/internal", (req, res) => {
+app.get("/internal", async (req, res) => {
+  const response = await axios.get("devx-namespace.local:80");
   return res.status(200).json({
     message: "calling from internal backend ms",
+    data: response.data,
   });
 });
 
